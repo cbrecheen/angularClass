6b7c4640499540af0a35010cb75d2a49efe75fee
Cole Brecheen <cbrecheen@extensis.com>
2016-12-06 11:25:38 -0800

    Made the active and inactive lists refresh when items are checked or unchecked.

--- src/people/people.component.ts
+++ src/people/people.component.ts
@@ -17,6 +17,7 @@ import { MyPersonComponent } from './person.component';
 })
 export class MyPeopleComponent {
     @Input() updatePerson; 
+    state: string = "all";
 
     peopleList = [];
     peopleService: PeopleService;
@@ -26,17 +27,33 @@ export class MyPeopleComponent {
     }
 
     getAllPeople() {
+        this.state = "all";
         this.peopleList = this.peopleService.getPeople();
     }
     getActivePeople() {
+        this.state = "active";
         this.peopleList = this.peopleService.getActivePeople();
     }
     getInactivePeople() {
+        this.state = "inactive";
         this.peopleList = this.peopleService.getInactivePeople();
     }
     onUpdatePerson(updatedPerson) {
-       this.peopleService.updatePerson(updatedPerson);
-       console.log(updatedPerson);        
+        this.peopleService.updatePerson(updatedPerson);
+        console.log(updatedPerson);
+        switch (this.state)
+        {
+        case "all":
+            this.getAllPeople();
+            break;        
+        case "active":
+            this.getActivePeople();
+            break;
+        case "inactive":
+            this.getInactivePeople();
+            break;
+
+        }        
     }
 }
 
